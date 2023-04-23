77b790500f4ffeeaf67585b89aa535c243474b40
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-04-23 00:36:51 -0300

    [feat]: create Button.stories

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@design-system-ignite-ui/react'
+
+export default {
+    title: 'Button',
+    component: Button,
+    args: {
+        children: 'Enviar'
+    }
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {
+    args: {}
+}
+
+export const Big: StoryObj<ButtonProps> = {
+    args: {
+        size: 'big',
+    }
+}

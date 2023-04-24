c0095bc2e3d56f52ed7f82f433200bc2f1656bf5
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-04-23 21:12:39 -0300

    [fix]: fix lint

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -2,19 +2,19 @@ import type { StoryObj, Meta } from '@storybook/react'
 import { Button, ButtonProps } from '@design-system-ignite-ui/react'
 
 export default {
-    title: 'Button',
-    component: Button,
-    args: {
-        children: 'Enviar'
-    }
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Enviar',
+  },
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {
-    args: {}
+  args: {},
 }
 
 export const Big: StoryObj<ButtonProps> = {
-    args: {
-        size: 'big',
-    }
+  args: {
+    size: 'big',
+  },
 }

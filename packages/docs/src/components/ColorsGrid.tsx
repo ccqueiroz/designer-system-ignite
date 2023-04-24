360179ca40d32211b27ed051c6db281163e03f36
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-04-23 21:53:09 -0300

    [feat]: adding doc to token colors

---
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -0,0 +1,21 @@
+import { colors } from '@design-system-ignite-ui/tokens'
+
+export function ColorsGrid() {
+  return Object.entries(colors)?.map(([key, color]) => {
+    return (
+      <div key={key} style={{ backgroundColor: color, padding: '2rem' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            fontFamily: 'monospace',
+            color: '#FFF',
+          }}
+        >
+          <strong>${key}</strong>
+          <strong>{color}</strong>
+        </div>
+      </div>
+    )
+  })
+}

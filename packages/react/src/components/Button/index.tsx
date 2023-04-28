737b3c13e58af371def11cba2565f73e4ca76199
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-04-28 14:28:28 -0300

    [refactor]: refactor Component Button put insider in folder Components

---
+++ packages/react/src/components/Button/index.tsx
@@ -0,0 +1,40 @@
+import React, { ComponentProps, ElementType } from 'react'
+import { styled } from '../../styles'
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$md',
+  padding: '$2 $4',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonComponentProps = {
+  children: React.ReactNode
+  size: 'small' | 'big'
+}
+
+export interface ButtonProps extends ComponentProps<typeof Button> {
+  as?: ElementType
+}
+
+Button.displayName = 'Button'

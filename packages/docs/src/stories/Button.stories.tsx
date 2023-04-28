daaaf79b0d3e5f1c3a12c35c7e375a50d6675f00
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-04-28 16:57:16 -0300

    [feat]: add controls in component Button

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,11 +1,24 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { Button, ButtonProps } from '@design-system-ignite-ui/react'
+import { ButtonProps, Button } from '@design-system-ignite-ui/react'
 
 export default {
   title: 'Form/Button',
   component: Button,
   args: {
     children: 'Enviar',
+    size: 'small',
+  },
+  argTypes: {
+    onClick: {
+      actinon: 'click',
+    },
+    size: {
+      description: 'Set a button size configuration.',
+      options: ['small', 'big'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<ButtonProps>
 
